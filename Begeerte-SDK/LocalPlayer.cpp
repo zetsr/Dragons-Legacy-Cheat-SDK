092af0a70a563e6b911fa9f8fa95b24a3a6aa983
zetsr <90092893+zetsr@users.noreply.github.com>
2025-03-24 04:22:37 +0800

    Delete Begeerte-SDK directory

--- Begeerte-SDK/LocalPlayer.cpp
+++
@@ -1,9 +0,0 @@
-#include "LocalPlayer.h"
-#include "Memory.h"
-
-namespace LocalPlayer {
-    Player* GetPlayer(DWORD64 baseAddress) {
-        if (!baseAddress) return nullptr;
-        return (Player*)baseAddress; // 直接返回结构指针
-    }
-}
