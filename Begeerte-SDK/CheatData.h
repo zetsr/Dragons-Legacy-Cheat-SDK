002071314b7bbc1618b1413046e22ced167263a2
zetsr <90092893+zetsr@users.noreply.github.com>
2025-03-24 18:17:24 +0800

    Add files via upload

---
+++ Begeerte-SDK/CheatData.h
@@ -0,0 +1,30 @@
+#ifndef CHEAT_DATA_H
+#define CHEAT_DATA_H
+
+#include "LocalPlayer.h"
+#include "Vector.h"
+
+struct CheatData {
+    Vector::Vec3 LocalCameraPosition;
+    LocalPlayer::Player* LocalPlayer;
+    int ClientCurrentTime;
+    int ClientCurrentTick;
+    Vector::Vec3 ClientViewAngles;
+    byte MaxHealth;
+    float RenderDistance; // FAKE
+};
+
+inline static CheatData cheatDataInstance = {
+    .LocalCameraPosition = Vector::Vec3(0, 0, 0),
+    .LocalPlayer = nullptr,
+    .ClientCurrentTime = 0,
+    .ClientCurrentTick = 0,
+    .ClientViewAngles = Vector::Vec3(0, 0, 0),
+    .MaxHealth = 200,
+    .RenderDistance = 50000.f
+};
+
+// 全局指针，指向静态实例
+inline static CheatData* g_cheatdata = &cheatDataInstance;
+
+#endif // CHEAT_DATA_H
