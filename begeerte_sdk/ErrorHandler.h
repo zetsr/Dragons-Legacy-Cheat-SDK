c2efa0eef603bb169ce620e756a65b7d11418463
zetsr <90092893+zetsr@users.noreply.github.com>
2025-04-11 16:04:40 +0800

    Delete begeerte_sdk directory

--- begeerte_sdk/ErrorHandler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <windows.h>
-
-namespace ErrorHandlerNs {
-    void RegisterExceptionFilter();
-    LONG WINAPI UnhandledExceptionFilter(EXCEPTION_POINTERS* ExceptionInfo);
-}
